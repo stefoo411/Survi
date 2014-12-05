20c1d33f7c9d46c9e18416f8af2dea608007f050
stefoo411 <stefoo411@users.noreply.github.com>
2014-12-05 18:48:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, redirect
 import jinja2
 import os
+from pymongo import *
+import os
 
 app = Flask(__name__)
 
+app.secret_key = 'kbwkfwbhwbhk'
+client = MongoClient('mongodb://survistefoo:survi@ds051110.mongolab.com:51110/survi')
+db = client.get_default_database()
+users = db.users
+
 @app.route('/')
 def hello():
+	#users.insert({'username':'paras2','password':'cool'})
 	return render_template("index.html")
 
 @app.route('/change')
@@ -17,6 +25,12 @@ def post():
 	if request.method == 'POST':
 		return render_template('post.html')
 	return render_template('get.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	users = db.users
+	users = users.find({})
+	return render_template('get.html',users=users)
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8000))
